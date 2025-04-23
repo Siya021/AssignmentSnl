1f0db768aab03ad70efaa3ca6adde2047ac3b2fe
Siyamthanda Mazokwana <129970054+Siya021@users.noreply.github.com>
2025-04-23 15:27:19 +0200

    Add project files.

---
+++ Assignment/NumberToWords.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class NumberToWords
+    {
+        public NumberToWords()
+        {
+        }
+
+        public static String[] Ones = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        public static String[] Teens = { 
+            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        public static String[] Tens = {
+            "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+        };
+        
+       
+        public static string ConvertToWords(int number)
+        {
+            if (number == 0)
+            {
+                return "zero";
+            }
+            string words = "";
+ 
+            if ((number / 1000) > 0)
+            {
+                words += ConvertToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+            if ((number / 100) > 0)
+            {
+                words += ConvertToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+            if (number > 0)
+            {
+                if (words != "")
+                {
+                    words += "and ";
+                }
+                if (number < 10)
+                {
+                    words += Ones[number];
+                }
+                else if (number < 20)
+                {
+                    words += Teens[number - 10];
+                }
+                else
+                {
+                    words += Tens[number / 10];
+                    if ((number % 10) > 0)
+                    {
+                        words += "-" + Ones[number % 10];
+                    }
+                }
+            }
+            return words.Trim();    
+        }
+
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Welcome to Assigment Pack 2");
+                Console.WriteLine("1. Number2Words - Assignment 3");
+                Console.WriteLine("2. Real World Object - Assignment 4");
+                Console.WriteLine("3. Return to Main Menu");
+                Console.WriteLine("Please select an option (1 or 2): ");
+
+                string selection = Console.ReadLine();
+
+                switch (selection)
+                {
+                    case "1":
+                        Console.WriteLine("Enter a number to convert to words:");
+                        if (int.TryParse(Console.ReadLine(), out int number))
+                        {
+                            string words = NumberToWords.ConvertToWords(number);
+                            Console.WriteLine($"The number in words is: {words}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter a valid number.");
+                        }
+                        break;
+                    case "2":
+                        RealWorldObject realWorldObject = new RealWorldObject();
+
+                        break;
+                    case "3":
+                        Console.WriteLine("Returning to Main Menu");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid selection. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
